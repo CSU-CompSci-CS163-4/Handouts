816bdc341d9c138244d6a2dcbd961f9478765cb8
Albert Lionelle <lionelle@gmail.com>
2021-09-23 20:01:56 -0600

    updated with some code practice

--- ClassExamples/src/YourProgram.java
+++ ClassExamples/src/YourProgram.java
@@ -1,56 +1,18 @@
 import java.util.Scanner;
 
 public class YourProgram {
-    Scanner  scanner = new Scanner(System.in); // instance!
 
-   // Write a program that reads 5 pairs (value1, value2) of int values. 
-    // For each pair print the smaller value. If the values are equal, print "equals" and the number.
-    //What do we need to change in our code if instead of 5 we want 100 pairs?
-   // What do we need to change in our code if instead of a predefined number we would like to ask for the 
-    // total number of pairs to read?
-
-    public void loopChallenge(){
-        int count = getInput("How many times do you want to loop? ");
-        for(int i = 0; i < count; i++) {
-            int val1 = getInput();
-            int val2 = getInput();
-            if(val1 < val2) {
-                System.out.println(val1);
-            }
-            if(val1 == val2) {
-                System.out.printf("Equals: %d", val1);
-            }
+    public static int findIndex(String str, Character ch, int start) {
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ch) return i;
         }
-      
-    }
-    
-    // essentially a quick way to ask the same  question
-    public int getInput() {
-        return  getInput("I need a number? "); 
-    }
-    //  so  I  don't have to repeat typing these  lines, a method makes sense
-    public int getInput(String question)  {
-        System.out.print(question);
-        return scanner.nextInt();
+        return -1;
     }
 
-    
     public static void main(String[] args) {
-      YourProgram example =  new YourProgram();
-      example.loopChallenge(); // because I  need scanner!
+        System.out.printf("TEST: the index is: %d", findIndex("SATOROTAS", 'A', 0));
+        System.out.printf("TEST: the index is: %d", findIndex("SATOROTAS", 'T', 3));
+        System.out.printf("TEST: the index is: %d", findIndex("SATOROTAS", 'Z', 4));
     }
-  
-    
 }
 
-/*
-  for(int i = 0; i < 5; i++) {
-        int val1 = getInput();
-        int val2 = getInput();
-        if(val1 < val2) {
-        System.out.println(val1);
-        }
-        if(val1 == val2) {
-        System.out.printf("Equals: %d", val1);
-        }
-        }*/
