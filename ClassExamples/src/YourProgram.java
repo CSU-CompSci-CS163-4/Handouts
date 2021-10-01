dcbd378d39d11164f820552eb82bb029d9767ff1
Albert Lionelle <lionelle@gmail.com>
2021-09-30 19:10:01 -0600

    added pass by value examples

--- ClassExamples/src/YourProgram.java
+++ ClassExamples/src/YourProgram.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
 public class YourProgram {
-
-    public static int findIndex(String str, Character ch, int start) {
-        for(int i = 0; i < str.length(); i++) {
-            if(str.charAt(i) == ch) return i;
-        }
-        return -1;
+    public static void modifyValues(Rectangle rectangle, int w, int l) {
+        rectangle.setLength(rectangle.getLength() / 2);
+        rectangle.setWidth(rectangle.getWidth() * 3 + 1 );
+        w = w / 2;
+        l = l * 3 + 1;
     }
-
+    public static void printValues(Rectangle rectangle, int w, int l) {
+        System.out.printf("Rectangle: Width %d, Length: %d, Area: %d%n",
+                rectangle.getWidth(), rectangle.getLength(), rectangle.getArea());
+        System.out.printf("Values of w: %d, of l: %d%n", w, l);
+        System.out.printf("Width == w? %b,  Length == l? %b%n%n",
+                rectangle.getWidth() == w, rectangle.getLength()==l);
+    }
     public static void main(String[] args) {
-        System.out.printf("TEST: the index is: %d", findIndex("SATOROTAS", 'A', 0));
-        System.out.printf("TEST: the index is: %d", findIndex("SATOROTAS", 'T', 3));
-        System.out.printf("TEST: the index is: %d", findIndex("SATOROTAS", 'Z', 4));
+        int w = 5;
+        int l = 10;
+        Rectangle rectangle = new Rectangle(w,l);
+        printValues(rectangle, w, l);
+        modifyValues(rectangle, w, l);
+        System.out.println("Values Modified!");
+        printValues(rectangle, w, l);
     }
 }
 
