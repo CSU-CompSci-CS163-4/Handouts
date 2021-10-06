16802078411cf9e71adb687ed2810a696988f972
Albert Lionelle <Albert.Lionelle@colostate.edu>
2021-10-06 12:52:26 -0600

    just updating minor changes from lecture

--- ClassExamples/src/DoublesFileReading.java
+++ ClassExamples/src/DoublesFileReading.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * @version 20211006
  */
@@ -5,8 +10,29 @@ public class DoublesFileReading {
 
 
     public static double[] fileReadingDouble(String filename) {
-        
-        
+        double[] values = new double[100];
+        int linecount = 0;
+        try {
+            Scanner scanner = new Scanner(new File(filename));
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                double ans = Double.parseDouble(line);
+                values[linecount++] = ans;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return copyArray(values, linecount);   
+    }
+    
+    private static double[] copyArray(double[] values, int max) {
+        double[] rtn = new double[max];
+        System.arraycopy(values, 0, rtn, 0, max);
+        return rtn;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(fileReadingDouble("doubles.txt")));
     }
     
     
