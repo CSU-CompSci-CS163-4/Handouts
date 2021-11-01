a39037ff46662228222f6144d4556635ca8c5712
Albert Lionelle <Albert.Lionelle@colostate.edu>
2021-10-31 20:02:57 -0600

    updated with inclass code

--- ClassExamples/src/RecursionExample.java
+++ ClassExamples/src/RecursionExample.java
@@ -1,4 +1,20 @@
 public class RecursionExample {
+    public static int simple_sum(int[] values) {
+        return simple_sum(values, 0);
+    }
+
+    public static int simple_sum(int[] values, int index) {
+            if (index >= values.length) return 0;
+            return values[index] + simple_sum(values, index + 1);
+
+    }
+
+   /* public static void main(String[] args) {
+        System.out.println(simple_sum(new int[]{}));
+       System.out.println(simple_sum(new int[]{ 1 }));
+       System.out.println(simple_sum(new int[]{1,2,3,4}));
+    }*/
+    
 
     public static int sum(Object[] values) {
         return sum(values, 0); // overload, for easier initial call
