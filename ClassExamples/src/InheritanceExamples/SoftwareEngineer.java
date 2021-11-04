f83b30029fcb6ff56c035f0057a5fa848f616d18
Albert Lionelle <Albert.Lionelle@colostate.edu>
2021-11-04 10:02:27 -0600

    Updated with softwareEngineer->arraylist examples

--- ClassExamples/src/InheritanceExamples/SoftwareEngineer.java
+++ ClassExamples/src/InheritanceExamples/SoftwareEngineer.java
@@ -1,5 +1,10 @@
 package InheritanceExamples;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class SoftwareEngineer extends Employee {
     private String jobCode = "None";
     public SoftwareEngineer(int id, String jobCode) {
@@ -11,20 +16,48 @@ public class SoftwareEngineer extends Employee {
         return "SE::" + jobCode;
     }
 
+    public static ArrayList<Employee> readFile() {
+        ArrayList<Employee> list = new ArrayList<>();
+        try {
+            Scanner scnner = new Scanner(new File("jobs.txt"));
+            while(scnner.hasNextLine()) {
+                String[] line = scnner.nextLine().split("\\s+");
+                if(line.length < 1) continue;
+                if(line.length > 1)
+                    list.add(new SoftwareEngineer(Integer.parseInt(line[0]), 
+                                line[1]));
+                else
+                    list.add(new Employee(Integer.parseInt(line[0])));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list; // FIX
+    }
 
     public static void main(String[] args) {
         SoftwareEngineer se = new SoftwareEngineer(123, "QRPLC");
         Employee hr = new Employee(1);
-        SoftwareEngineer se2 = new SoftwareEngineer(345, "AI9");
+        //SoftwareEngineer se2 = new SoftwareEngineer(345, "AI9");
         /*String format = "Employee id: %-4s Job Code: %s%n";*/
-        System.out.println(se);
+        /*System.out.println(se);
         System.out.println(hr);
-        System.out.println(se2);
+        System.out.println(se2);*/
+       /* ArrayList<Employee> list = new ArrayList<>();
+        list.add(hr);
+        list.add(se);*/
+        ArrayList<Employee> list = readFile();
+        list.add(new SoftwareEngineer(345, "AI9"));
+        for(Employee em : list)  {
+            System.out.println(em);
+        }
         /*
          * System.out.printf(format, se.getID(), se.getJobCode());
          * System.out.printf(format, hr.getID(), hr.getJobCode());
          * System.out.printf(format, se2.getID(), se2.getJobCode());
          */
+        
     }
 
 }
