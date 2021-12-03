c34aa509c049f12161757b3d627699846ce432ca
Albert Lionelle <lionelle@gmail.com>
2021-12-02 19:58:20 -0700

    updated with examples for students to download.

---
+++ ClassExamples/src/CollectionsHashmapExample.java
@@ -0,0 +1,20 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class CollectionsHashmapExample {
+    public static void main(String args []){
+        Map<String, Double> mapCountry = new HashMap<String, Double>();
+        mapCountry.put("Brazil",3288000.0);
+        mapCountry.put("USA", 3797000.0);
+        mapCountry.put("Portugal", 35603.0);
+        mapCountry.put("India", 1269000.0);
+        System.out.println(mapCountry);
+        //how to return a list of keys?
+        //System.out.println(keys);
+        //how to return a list of values?
+        //System.out.println(values);
+    }
+}
+
---
+++ ClassExamples/src/CollectionsLinkedListExample.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+
+public class CollectionsLinkedListExample {
+
+    public static void main(String args[]){
+        List<Integer> list = new LinkedList<>();
+        list.add(2);
+        list.add(4);
+        list.add(6);
+        ListIterator<Integer> intIterator = list.listIterator();
+        //what is printed?
+        while(intIterator.hasNext()){
+            System.out.print(intIterator.next());
+        }
+        System.out.println("");
+         //how to add number 5 between numbers 4 and 6?
+         //how to add number 1 before number 2?
+         //how to remove number 2?
+         //how to remove the second element in the list?
+         //what is printed?
+        intIterator = list.listIterator();
+        while(intIterator.hasNext()){
+            System.out.print(intIterator.next());
+        }
+        System.out.println();
+        for(Integer val : list) { // yes this is the same as above
+            System.out.print(val);
+        }
+       }
+}
