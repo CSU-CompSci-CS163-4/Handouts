c47f6154bba2867f50e9dfcaf7b2d85377443db5
Albert Lionelle <albert.lionelle@colostate.edu>
2022-01-25 12:08:17 -0700

    First pass at week 5, Friday lecture

---
+++ ClassExamples/05InterfacesJavadocUml/Examples.java
@@ -0,0 +1,14 @@
+public class Examples {
+
+
+    public static void main(String[] args) {
+        String clientInput = "   hello   ";
+        clientInput = clientInput.trim();
+        // client input is now "hello"
+
+        boolean checkbox = true;
+        String print = String.valueOf(checkbox);
+        // sets String "true" to variable print
+    }
+    
+}
