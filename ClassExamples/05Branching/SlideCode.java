6dc14fede8fdeaf7e3ed2a195b1a299136e41f41
Albert Lionelle <albert.lionelle@colostate.edu>
2022-01-30 13:49:26 -0700

    update for monday lecture

---
+++ ClassExamples/05Branching/SlideCode.java
@@ -0,0 +1,25 @@
+public class SlideCode {
+
+
+/*    public static void main(String[] args) {
+        boolean myVal = 10 > 2;
+        System.out.println(myVal); // prints true to the screen
+    } */
+
+    /*
+    public static void main(String[] args) {
+        boolean myVal = 10 <= (20 * 2) + 1 / 2 - 39;
+        System.out.println(myVal); // prints false to the screen
+    } */
+
+    public static void main(String[] args) {
+        System.out.println(myCoolMethod()); // prints true
+
+
+    }
+
+    public static boolean myCoolMethod() {
+        return 10.5 < 20;
+    }
+    
+}
