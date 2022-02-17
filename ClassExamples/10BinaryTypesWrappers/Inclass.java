4b4bbdad158df52d37e3a210a4b57e7d90867190
Albert Lionelle <albert.lionelle@colostate.edu>
2022-02-17 15:13:43 -0700

    Friday slide update

---
+++ ClassExamples/10BinaryTypesWrappers/Inclass.java
@@ -0,0 +1,29 @@
+public class Inclass {
+
+
+    // TASK ONE
+    public static String convertToDigits(String str) {
+        String newString = "";
+        // write a loop here
+        // useful methods are Character.isDigit(), str.charAt(num), str.length()
+
+        return newString;
+    }
+
+   //Task two
+    public static double stringsToDouble(String left, String right) {
+        // takes left side to be the left side so left.right 
+        // converts to a double and returns the double value
+
+        return 0 ; // change
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertToDigits("811-111-111"));
+
+        double answer = stringsToDouble("111", "456");
+        System.out.println(answer);
+        System.out.println(answer/2);
+    }
+    
+}
