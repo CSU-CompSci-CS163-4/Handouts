b802e9e76bcbea14dc2ddbf9b2b35422e74aea63
Albert Lionelle <albert.lionelle@colostate.edu>
2022-02-20 10:31:53 -0700

    Updated with Monday slides

---
+++ ClassExamples/11ArrayLists/InClassMain.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class InClassMain {
+
+
+
+    // Activity One
+    // Create an ArrayList  
+    // populate it with the names of the people at the table
+    // print out the ArrayList, as a list
+    // return the list
+    public static ArrayList<String> task1() {
+
+        return null; // change this to be your ArrayList variable name
+
+    }
+
+
+    // Activity 2, three parts
+    // write two loops
+    // one that prints every name of the format "index: name"
+    // one that prints every *other* name of the format "index: name" (see slides)
+    // one that prints the .length() of every String in the list. 
+    public static void task2(ArrayList<String> names) {
+       
+    }
+
+    // Activity 3
+    // Write a loop (using a for:each), that prints out the following for every item in your ArrayList you have been using. 
+    // format:
+    // Name is N characters long, and the first character is X and the middle character is Y and the last character is Z.
+    // example
+    // Link is 4 characters long, and the first character is L, and the middle character is n, and the last character is k.
+    // Some useful String methods to help out:
+    // * .length()
+    // * .charAt(int)
+    public static void task3(ArrayList<String> names) {
+
+    }
+
+
+    public static void main(String[] args) {
+       ArrayList<String> names = task1();
+       task2(names);
+       task3(names);
+    }
+    
+}
