96e0876f312911667a4eb4f6d251790837a6e471
Albert Lionelle <albert.lionelle@colostate.edu>
2022-03-13 13:40:15 -0600

    Slides updated for exceptions

---
+++ slides/book/Unit3/data/Myprogram.java
@@ -0,0 +1,10 @@
+public class Myprogram {
+    // this is the first comment
+
+    public static void main(String[] args) {
+        // in the main program
+        System.out.println("This line should be ignored");
+        //another comment
+        System.out.println("This is another line"); // and a comment at the end
+    }
+}
