4e7b3e6ec0f90dfab2b35d189783bb2565614a31
Marcia Moraes <marcia.moraes@colostate.edu>
2023-03-23 16:57:02 -0600

    Cake class updated

--- ClassExamples/09MoreClasses/Cake.java
+++ ClassExamples/09MoreClasses/Cake.java
@@ -1,45 +1,25 @@
 public class Cake {
-    public static boolean EAT_IT = true; // Label 1
-    private String name; // Label 2
+    public static final boolean IS_GOOD = true;
+    private String name;
     private double cost;
 
-    public Cake() { // Label 3
-        this("Chocolate", 0.0);
-    }
-
-    public Cake(String name, double cost) { // Label 4
-        setName(name);
-        setCost(cost);
-    }
-
-    // Label 5
     public void setName(String str) {
         name = str;
     }
-
     public String getName() {
         return name;
     }
-
     public void setCost(double cost) {
         this.cost = cost;
     }
-
     public double getCost() {
         return this.cost;
     }
-
-    public static void main(String[] args) {
-        Cake coconut = new Cake("Coconut", 1.0);
-        Cake chocolate = new Cake();
-
-        double save = chocolate.getCost();
-        chocolate.setCost(coconut.getCost());
-        coconut.setCost(save);
-
-        coconut.setName(chocolate.getName()+ " " +coconut.getName());
-        
-        System.out.println(coconut.getCost());
-        System.out.println(coconut.getName());
+    public Cake(){
+        this("", 0);
+    }
+    public Cake(String name, double cost) {
+        setName(name);
+        setCost(cost);
     }
 }
