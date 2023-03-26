ce7233a93fe80eab007e09b1aef89bea47661018
Marcia Moraes <marcia.moraes@colostate.edu>
2023-03-26 17:19:24 -0600

    Add files via upload

---
+++ ClassExamples/10Polymorphism/Shape.java
@@ -0,0 +1,14 @@
+public abstract class Shape {
+    protected int sides;
+    private String color;
+    public Shape(int sides, String color) {
+        this.sides = sides;
+        setColor(color);
+    }
+    public String getColor() { return color;}
+    public int getSides() {return sides;} // added from the UML
+    public void setColor(String color) {this.color = color;}
+    public String toString() {
+                return String.format("Sides: %d, Color: %s", sides, color);
+    }
+}
