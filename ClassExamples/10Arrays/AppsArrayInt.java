1d8fff96ca65e5801e32d0f1e3d095b6708464fa
Marcia Moraes <marcia.moraes@colostate.edu>
2023-03-28 19:15:35 -0600

    Add files via upload

---
+++ ClassExamples/10Arrays/AppsArrayInt.java
@@ -0,0 +1,7 @@
+public class AppsArrayInt {
+    public static void main(String args[]){
+        ArrayInts obj = new ArrayInts(8);
+        obj.readArray();
+        System.out.println(obj);
+    }
+}
